133fb816f5e88ca67cac8b15b10c795d48afb30d
komangsuryasedana <komangsuryasedana@mail.com>
2024-03-14 15:01:57 +0800

    fix: eslint code

--- components/forms/form-textarea.tsx
+++ components/forms/form-textarea.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import { KeyboardEventHandler, forwardRef } from "react";
 import { Label } from "@/components/ui/label";
@@ -7,63 +7,69 @@ import { cn } from "@/lib/utils";
 import { FormErrors } from "./form-errors";
 import { useFormStatus } from "react-dom";
 
-
 interface FormTextAreaProps {
-    id: string;
-    label?: string;
-    placeholder?: string;
-    required?: boolean;
-    disabled?: boolean;
-    errors?: Record<string, string[] | undefined>;
-    className?: string;
-    onBlur?: () => void;
-    onClick?: () => void;
-    onKeyDown?: KeyboardEventHandler<HTMLTextAreaElement> | undefined;
-    defaultValue?: string
+  id: string;
+  label?: string;
+  placeholder?: string;
+  required?: boolean;
+  disabled?: boolean;
+  errors?: Record<string, string[] | undefined>;
+  className?: string;
+  onBlur?: () => void;
+  onClick?: () => void;
+  onKeyDown?: KeyboardEventHandler<HTMLTextAreaElement> | undefined;
+  defaultValue?: string;
 }
-export const FormTextArea = forwardRef<HTMLTextAreaElement, FormTextAreaProps>(({
-    id,
-    label,
-    placeholder,
-    required,
-    disabled,
-    errors,
-    className,
-    onBlur,
-    onClick,
-    onKeyDown,
-    defaultValue
-}, ref) => {
+
+export const FormTextArea = forwardRef<HTMLTextAreaElement, FormTextAreaProps>(
+  function FormTextAreaComponent(
+    {
+      id,
+      label,
+      placeholder,
+      required,
+      disabled,
+      errors,
+      className,
+      onBlur,
+      onClick,
+      onKeyDown,
+      defaultValue,
+    },
+    ref
+  ) {
     const { pending } = useFormStatus();
-  return (
-    <div className="space-y-2 w-full">
-      <div className="w-full space-y-1">
-        {label ? (
-          <Label
-            className="text-xs font-semibold text-neutral-700"
-            htmlFor={id}
-          >
-            {label}
-          </Label>
-        ) : null}
-        <Textarea
-          onKeyDown={onKeyDown}
-          id={id}
-          name={id}
-          ref={ref}
-          placeholder={placeholder}
-          required={required}
-          disabled={pending || disabled}
-          className={cn(
-            "resize-none focus-visible:ring-0 focus-visible:ring-offset-0 focus:ring-0 ring-0 outline-none shadow-sm", className
-          )}
-          aria-describedby={`${id}-error`}
-          defaultValue={defaultValue}
-          onBlur={onBlur}
-          onClick={onClick}
-        />
+    return (
+      <div className="space-y-2 w-full">
+        <div className="w-full space-y-1">
+          {label ? (
+            <Label
+              className="text-xs font-semibold text-neutral-700"
+              htmlFor={id}
+            >
+              {label}
+            </Label>
+          ) : null}
+          <Textarea
+            onKeyDown={onKeyDown}
+            id={id}
+            name={id}
+            ref={ref}
+            placeholder={placeholder}
+            required={required}
+            disabled={pending || disabled}
+            className={cn(
+              "resize-none focus-visible:ring-0 focus-visible:ring-offset-0 focus:ring-0 ring-0 outline-none shadow-sm",
+              className
+            )}
+            aria-describedby={`${id}-error`}
+            defaultValue={defaultValue}
+            onBlur={onBlur}
+            onClick={onClick}
+          />
+        </div>
+        <FormErrors id={id} errors={errors} />
       </div>
-      <FormErrors id={id} errors={errors} />
-    </div>
-  );
-});
+    );
+  }
+);
